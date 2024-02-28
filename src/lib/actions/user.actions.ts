e689a4c26d72f55848f19afb42f76fd95179d8d2
ArshdeepBrar300 <arshdeepbrar171@gmail.com>
2024-02-28 23:02:28 +0530

    clerk mongodb connection

--- src/lib/actions/user.actions.ts
+++ src/lib/actions/user.actions.ts
@@ -8,7 +8,11 @@ import {handleError} from '../utils'
 export async function createUser(user:CreateUserParams){
     try{
         await connectToDatabase()
+        console.log('connected to database');
+        
         const newUser=await User.create(user)
+        console.log('--- user db working');
+        
         return JSON.parse(JSON.stringify(newUser))
 
     }
